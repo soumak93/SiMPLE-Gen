c1849e1daca0d44bf36ba2b825bab21134b12cea
soumak93 <soumak93@gmail.com>
2025-01-01 21:33:05 +0530

    halo_sightlines.py and optical_depth.py modified

--- lib/halo_sightlines.py
+++ lib/halo_sightlines.py
@@ -1,132 +1,144 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan  1 19:09:28 2025
+Script for reading simulation data and generating halo sightlines.
 
-@author: soumak
+Created on Wed Jan 1, 2025
+@author: Soumak
 """
-
 
 import numpy as np
 from astropy import units as u
-from astropy.constants import c,m_p,m_e,k_B
+from astropy.constants import c
 from astropy.cosmology import Planck18 as cosmo
-import time
 
+def read_field(file_directory, n_grid):
+    """
+    Reads density, temperature, ionization fraction, and velocity fields 
+    from specified files.
 
+    Parameters:
+        file_directory (str): Path to the directory containing the data files.
+        n_grid         (int): Number of grid points along one axis of the 3D grid.
 
+    Returns:
+        tuple: Density, temperature, ionization fraction, and velocity fields.
+    """
+    file_density    = f"{file_directory}cube_D_2048_instantanne_17.dat"
+    file_temp       = f"{file_directory}cubeT/cube_T_2048_instantanne_17.dat"
+    file_ion_frac   = f"{file_directory}cubeX/cube_X_2048_instantanne_17.dat"
+    file_velocity   = f"{file_directory}grid_017/velx_field.npy"
 
-def field_read(file_directory, n_grid):
+    Dbox            = np.fromfile(file_density, dtype=np.float64, count=n_grid**3).reshape((n_grid, n_grid, n_grid)).astype(np.float32)
+    print("Density field read...")
 
-    file_path1 = file_directory+"cube_D_2048_instantanne_17.dat"
-    data_type = np.float64  # Replace with the actual data type of your file, if known.
-    Dbox = np.fromfile(file_path1, dtype=data_type,count=n_grid**3)
-    Dbox = np.reshape(Dbox,(n_grid,n_grid,n_grid)).astype(np.float32)
-    print("Density read...")
-    
-    file_path2 = file_directory+"cubeT/cube_T_2048_instantanne_17.dat"
-    data_type = np.float64  # Replace with the actual data type of your file, if known.
-    Tbox = np.fromfile(file_path2, dtype=data_type,count=n_grid**3)
-    Tbox = np.reshape(Tbox,(n_grid,n_grid,n_grid)).astype(np.float32)
-    print("Temperature read...")
-    
-    file_path3 = file_directory+"cubeX/cube_X_2048_instantanne_17.dat"
-    data_type = np.float64  # Replace with the actual data type of your file, if known.
-    Xbox = np.fromfile(file_path3, dtype=data_type,count=n_grid**3)
-    Xbox = np.reshape(Xbox,(n_grid,n_grid,n_grid))
-    print("Ionization fraction read...")
-    
-    Vbox = np.load(file_directory+"grid_017/velx_field.npy").astype(np.float32)
-    print("Velocity read...")
-    
+    Tbox            = np.fromfile(file_temp, dtype=np.float64, count=n_grid**3).reshape((n_grid, n_grid, n_grid)).astype(np.float32)
+    print("Temperature field read...")
+
+    Xbox            = np.fromfile(file_ion_frac, dtype=np.float64, count=n_grid**3).reshape((n_grid, n_grid, n_grid))
+    print("Ionization fraction field read...")
+
+    Vbox            = np.load(file_velocity).astype(np.float32)
+    print("Velocity field read...")
+
     return Dbox, Tbox, Xbox, Vbox
-    
 
-def halo_read(file_directory):
-    
-    with open(file_directory+"halo_catalog_015.dat", 'rb') as f:
-        nhalo = np.fromfile(f, dtype=np.int32, count=1)
-        halomass = np.fromfile(f, dtype=np.float32, count=nhalo[0])  # Msun 
-        halo_cm = np.fromfile(f, dtype=np.float32, count=3*nhalo[0]) # Mpc/h
-        
-    halo_cm = halo_cm.reshape((nhalo[0],3))
-    
-    print("halo catalog read...")
-    
+def read_halo_catalog(file_directory):
+    """
+    Reads halo catalog data.
+
+    Parameters:
+        file_directory (str): Path to the directory containing the halo catalog file.
+
+    Returns:
+        tuple: Number of halos, halo masses, and halo center-of-mass positions.
+    """
+    file_halo_catalog = f"{file_directory}halo_catalog_015.dat"
+
+    with open(file_halo_catalog, 'rb') as f:
+        nhalo      = np.fromfile(f, dtype=np.int32, count=1)
+        halomass   = np.fromfile(f, dtype=np.float32, count=nhalo[0])  # Halo masses (Msun)
+        halo_cm    = np.fromfile(f, dtype=np.float32, count=3 * nhalo[0]).reshape((nhalo[0], 3))  # Positions (Mpc/h)
+
+    print("Halo catalog read...")
+
     return nhalo, halomass, halo_cm
-    
 
+def generate_halo_sightlines(
+    Dbox, Tbox, Xbox, Vbox, nhalo, halomass, halo_cm, 
+    halomass_cutoff, z_centre, BoxSize, Outfile_dir
+):
+    """
+    Generates sightlines for halos and saves results to specified directory.
 
+    Parameters:
+        Dbox            (np.ndarray): Density field.
+        Tbox            (np.ndarray): Temperature field.
+        Xbox            (np.ndarray): Ionization fraction field.
+        Vbox            (np.ndarray): Velocity field.
+        nhalo           (np.ndarray): Number of halos.
+        halomass        (np.ndarray): Halo masses.
+        halo_cm         (np.ndarray): Halo positions (center of mass).
+        halomass_cutoff (float)     : Minimum halo mass to include.
+        z_centre        (float)     : Central redshift of the simulation.
+        BoxSize         (float)     : Physical size of the simulation box (Mpc/h).
+        Outfile_dir     (str)       : Directory to save the output data.
 
-def halo_sightlines_gen(Dbox, Tbox, Xbox, Vbox, nhalo, halomass, halo_cm, halomass_cutoff, z_centre, BoxSize, Outfile_dir):
-    
-    ###############################################################################
-    # Length conversions
-    ###############################################################################
+    Returns:
+        None
+    """
+    Boxlen        = Dbox.shape[0]
+    i_center      = Boxlen // 2
+    x_sim         = np.linspace(0., BoxSize, Boxlen, endpoint=False)
+    x             = x_sim * u.Mpc / cosmo.h
 
-    Boxlen  = len(Dbox[:,0,0])
-    i_center = int(Boxlen/2)-1
+    H_z           = cosmo.H(z_centre)
+    dx_dz         = c / H_z
+    z             = np.zeros_like(x)
+    z[0]          = z_centre - (x[i_center] - x[0]) / dx_dz
 
+    for i in range(len(z) - 1):
+        z[i + 1] = z[i] + (x[i + 1] - x[i]) / dx_dz
 
-    x_sim   = np.linspace(0.,BoxSize,Boxlen+1)[:-1] 
+    wavelength    = (1 + z) * 1215.67 * u.Angstrom
 
-    x       = x_sim * u.Mpc / cosmo.h
+    # Compute neutral hydrogen number density
+    nHbox         = Dbox / (u.m.to(u.cm) / (1.0 + z_centre))**3
+    nHIbox        = nHbox * (1 - Xbox)
 
-    H_z     = cosmo.H(z_centre)
-    dx_dz   = c / H_z 
+    # Filter halos by mass cutoff
+    valid_halos   = halomass > halomass_cutoff
+    halo_cm       = halo_cm[valid_halos]
+    halomass      = halomass[valid_halos]
 
-    z       = np.zeros(len(x))
-    z[0]    = z_centre - (x[int(Boxlen/2)]-x[0])/dx_dz
+    halo_pos_grid = (halo_cm * Boxlen / BoxSize).astype(int)
+    n_HI_halo     = nHIbox[:, halo_pos_grid[:, 1], halo_pos_grid[:, 2]].T
+    T_halo        = Tbox[:, halo_pos_grid[:, 1], halo_pos_grid[:, 2]].T
+    v_pec_halo    = Vbox[:, halo_pos_grid[:, 1], halo_pos_grid[:, 2]].T
 
-    for i in range(len(z)-1):
-        z[i+1] = z[i] + (x[i+1]-x[i])/dx_dz
-        
-    wavelength = (1+z) * 1215.67
-    
-    
-    ###############################################################################
-    # nHI
-    ###############################################################################
+    # Shift data to center on each halo
+    shifts        = i_center - halo_pos_grid[:, 0]
+    n_HI_halo     = np.roll(n_HI_halo, shift=shifts, axis=1)
+    T_halo        = np.roll(T_halo, shift=shifts, axis=1)
+    v_pec_halo    = np.roll(v_pec_halo, shift=shifts, axis=1)
 
-    nHbox = Dbox/(u.m.to(u.cm)/(1.0+z_centre))**3
-    nHIbox = (nHbox * (1-Xbox) )
-    del Xbox, Dbox
-    
-    ###############################################################################
-    
-    halo_cm = halo_cm[(halomass>halomass_cutoff),:]
-    halomass= halomass[(halomass>halomass_cutoff)]
-
-    halo_pos_grid = (halo_cm*Boxlen/BoxSize).astype(int)
-
-    n_HI_halo = nHIbox[:,halo_pos_grid[:,1],halo_pos_grid[:,2]].T
-    T_halo = Tbox[:,halo_pos_grid[:,1],halo_pos_grid[:,2]].T
-    v_pec_halo = Vbox[:,halo_pos_grid[:,1],halo_pos_grid[:,2]].T
-
-    n_HI_halo = np.roll(n_HI_halo,shift=(i_center-halo_pos_grid[:,0]),axis=1)
-    T_halo = np.roll(T_halo,shift=(i_center-halo_pos_grid[:,0]),axis=1)
-    v_pec_halo = np.roll(v_pec_halo,shift=(i_center-halo_pos_grid[:,0]),axis=1)
-
-
-
-    n_HI_halo = n_HI_halo[:,int(i_center - Boxlen/8):int(i_center + Boxlen/8)]
-    T_halo = T_halo[:,int(i_center - Boxlen/8):int(i_center + Boxlen/8)]
-    v_pec_halo = v_pec_halo[:,int(i_center - Boxlen/8):int(i_center + Boxlen/8)]
-    z = z[int(i_center - Boxlen/8):int(i_center + Boxlen/8)]
-    x_sim = x_sim[int(i_center - Boxlen/8):int(i_center + Boxlen/8)]
+    # Truncate to a region around the center
+    region_slice  = slice(int(i_center - Boxlen / 8), int(i_center + Boxlen / 8))
+    n_HI_halo     = n_HI_halo[:, region_slice]
+    T_halo        = T_halo[:, region_slice]
+    v_pec_halo    = v_pec_halo[:, region_slice]
+    z             = z[region_slice]
+    x_sim         = x_sim[region_slice]
 
     print("Halo sightlines extracted...")
 
-    np.save(Outfile_dir+"n_HI_halo_grid_017_M9_5.npy",n_HI_halo)
-    np.save(Outfile_dir+"T_halo_grid_017_M9_5.npy",T_halo)
-    np.save(Outfile_dir+"v_pec_halo_grid_017_M9_5.npy",v_pec_halo)
-    np.save(Outfile_dir+"halocm_grid_017_M9_5.npy",halo_cm)
-    np.save(Outfile_dir+"halomass_grid_017_M9_5.npy",halomass)
-    np.save(Outfile_dir+"x_sim_grid_017_M9_5.npy",x_sim)
-    np.save(Outfile_dir+"z_grid_017_M9_5.npy",z)
-    
-    
-
-
+    # Save results
+    np.save(f"{Outfile_dir}n_HI_halo.npy",    n_HI_halo)
+    np.save(f"{Outfile_dir}T_halo.npy",       T_halo)
+    np.save(f"{Outfile_dir}v_pec_halo.npy",   v_pec_halo)
+    np.save(f"{Outfile_dir}halo_cm.npy",      halo_cm)
+    np.save(f"{Outfile_dir}halo_mass.npy",    halomass)
+    np.save(f"{Outfile_dir}x_sim.npy",        x_sim)
+    np.save(f"{Outfile_dir}z.npy",            z)
 
 
--- lib/optical_depth.py
+++ lib/optical_depth.py
@@ -1,125 +1,156 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan  1 20:22:00 2025
+Script to calculate optical depth for halo sightlines.
 
-@author: soumak
+This script contains functions to compute the optical depth for
+Lyman-alpha absorption in the sightlines of halos, leveraging
+cosmological and astrophysical parameters.
+
+Author: Soumak
+Created: Jan 1, 2025
 """
 
 from halo_sightlines import *
+from scipy.interpolate import interp1d
+from scipy import special
+import numpy as np
+import time
+from astropy.constants import c, m_p, m_e, k_B
+from astropy import units as u
+from astropy.cosmology import Planck18 as cosmo
 
 ###############################################################################
-# Optical Depth
+# Optical Depth Calculation Functions
 ###############################################################################
-
 
 def tau(x, z, n_HI, T, v_pec):
-    I = 4.45 * (10.0 ** -18)
-    x = u.Mpc.to(u.cm) * x / cosmo.h
-    lambda_lu = 1215.67 * (10.0 ** -8)
-    nu_lu = c.to(u.cm / u.s).value / lambda_lu
-    gamma_ul = 6.262 * (10.0 ** 8)
-    m_H = (m_p + m_e).to(u.g).value
-    
-    # Compute b, dx, and term1 across all samples
-    b = np.sqrt(2.0 * k_B.to(u.erg / u.K).value * T / m_H) 
-    dx = np.mean(x[1:] - x[:-1])
-    term1 = (c.to(u.cm / u.s).value * I / np.sqrt(np.pi)) * dx * (n_HI / (b * (1 + z))) 
-    
-    z_expanded = z[None, :, None] 
-    b_expanded = b[:, :, None]   
+    """
+    Calculate optical depth (tau) for Lyman-alpha absorption.
 
-    term2 = np.real(
+    Parameters
+    ----------
+    x : ndarray
+        Proper distance array (in Mpc).
+    z : ndarray
+        Redshift array.
+    n_HI : ndarray
+        Neutral hydrogen number density array.
+    T : ndarray
+        Temperature array (in Kelvin).
+    v_pec : ndarray
+        Peculiar velocity array (in km/s).
+
+    Returns
+    -------
+    tau_z : ndarray
+        Optical depth as a function of redshift.
+    """
+    I          = 4.45e-18  # Intensity constant
+    x          = u.Mpc.to(u.cm) * x / cosmo.h
+    lambda_lu  = 1215.67e-8  # Lyman-alpha wavelength in cm
+    nu_lu      = c.to(u.cm / u.s).value / lambda_lu
+    gamma_ul   = 6.262e8  # Damping coefficient in Hz
+    m_H        = (m_p + m_e).to(u.g).value
+
+    # Compute Doppler width (b), spacing (dx), and term1
+    b          = np.sqrt(2.0 * k_B.to(u.erg / u.K).value * T / m_H)
+    dx         = np.mean(x[1:] - x[:-1])
+    term1      = (c.to(u.cm / u.s).value * I / np.sqrt(np.pi)) * dx * (n_HI / (b * (1 + z)))
+
+    # Expand dimensions for vectorized computation
+    z_expanded = z[None, :, None]
+    b_expanded = b[:, :, None]
+
+    term2      = np.real(
         special.wofz(
             1j * gamma_ul * c.to(u.cm / u.s).value / (4 * np.pi * nu_lu * b_expanded)
-            + c.to(u.cm / u.s).value * (z_expanded - z[None,None,:]) / (b_expanded * (1 + z[None,None,:]))
-            + v_pec[:, :, None] * 10.0 ** 5 / b_expanded
+            + c.to(u.cm / u.s).value * (z_expanded - z[None, None, :]) / (b_expanded * (1 + z[None, None, :]))
+            + v_pec[:, :, None] * 1e5 / b_expanded
         )
-    )  
-    
-    tau_z = np.sum(term1[:, :, None] * term2, axis=1)
-    
+    )
+
+    tau_z      = np.sum(term1[:, :, None] * term2, axis=1)
+
     return tau_z
 
-def tau_halo_sightline(file_dir,z_centre,N_bunch=8192):
-    x_sim=np.load(file_dir+"/x_sim_grid_023_M9_5.npy")[(255-48):(255+48)]
-    z=np.load(file_dir+"/z_grid_023_M9_5.npy")[(255-48):(255+48)]
+def tau_halo_sightline(file_dir, z_centre, N_bunch=8192):
+    """
+    Generate optical depth (tau) for halo sightlines.
 
-    x_sim_new = np.linspace(np.min(x_sim),np.max(x_sim),2*len(x_sim))
-    z = np.linspace(np.min(z),np.max(z),2*len(z))
+    Parameters
+    ----------
+    file_dir : str
+        Directory containing input data files.
+    z_centre : float
+        Central redshift.
+    N_bunch : int, optional
+        Number of halos to process in each batch. Default is 8192.
 
+    Returns
+    -------
+    Saves computed tau data to files in the specified directory.
+    """
+    # Load and refine data
+    x_sim   = np.load(file_dir + "/x_sim_grid_023_M9_5.npy")[(255 - 48):(255 + 48)]
+    z       = np.load(file_dir + "/z_grid_023_M9_5.npy")[(255 - 48):(255 + 48)]
 
-    n_HI_halo=np.load(file_dir+"/n_HI_halo_grid_023_M9_5.npy")[:,(255-48):(255+48)]
-    T_halo=np.load(file_dir+"/T_halo_grid_023_M9_5.npy")[:,(255-48):(255+48)]
-    v_pec_halo=np.load(file_dir+"/v_pec_halo_grid_023_M9_5.npy")[:,(255-48):(255+48)]
-    halomass=np.load(file_dir+"/halomass_grid_023_M9_5.npy")
+    x_sim_new = np.linspace(np.min(x_sim), np.max(x_sim), 2 * len(x_sim))
+    z         = np.linspace(np.min(z), np.max(z), 2 * len(z))
 
-    f_n_HI = interp1d(x_sim, n_HI_halo, kind='linear', axis=1)
-    n_HI_halo = f_n_HI(x_sim_new)
-    f_T = interp1d(x_sim, T_halo, kind='linear', axis=1)
-    T_halo = f_T(x_sim_new)
-    f_v_pec = interp1d(x_sim, v_pec_halo, kind='linear', axis=1)
-    v_pec_halo = f_v_pec(x_sim_new)
-    
-    i_center = int(len(n_HI_halo[0,:])/2)-1
-    
-    vpos_halos_final = np.arange(-500,501,2.0)
+    n_HI_halo = np.load(file_dir + "/n_HI_halo_grid_023_M9_5.npy")[:, (255 - 48):(255 + 48)]
+    T_halo    = np.load(file_dir + "/T_halo_grid_023_M9_5.npy")[:, (255 - 48):(255 + 48)]
+    v_pec_halo = np.load(file_dir + "/v_pec_halo_grid_023_M9_5.npy")[:, (255 - 48):(255 + 48)]
+    halomass  = np.load(file_dir + "/halomass_grid_023_M9_5.npy")
 
-    F_halo_final = np.zeros((len(halomass),len(vpos_halos_final))) #np.load("/home/soumak/My_Files/Lya-Emitters/TIFR/tau_halo_M_9_5_grid_023.npy")
+    # Interpolate to refined grid
+    n_HI_halo = interp1d(x_sim, n_HI_halo, kind='linear', axis=1)(x_sim_new)
+    T_halo    = interp1d(x_sim, T_halo, kind='linear', axis=1)(x_sim_new)
+    v_pec_halo = interp1d(x_sim, v_pec_halo, kind='linear', axis=1)(x_sim_new)
+
+    i_center  = int(len(n_HI_halo[0, :]) / 2) - 1
+    vpos_halos_final = np.arange(-500, 501, 2.0)
+
+    F_halo_final = np.zeros((len(halomass), len(vpos_halos_final)))
 
     halo_n = N_bunch
 
-    for sec in range(int(len(halomass)/halo_n)+1):
-        print("Progress="+str(100.0*sec/int(len(halomass)/halo_n))+"%")
-        
-        
-        if int((sec+1)*halo_n) < len(halomass):
-            halo_indices = np.arange(int(sec*halo_n),int((sec+1)*halo_n),1)
+    for sec in range(int(len(halomass) / halo_n) + 1):
+        print(f"Progress: {100.0 * sec / int(len(halomass) / halo_n):.2f}%")
+
+        if int((sec + 1) * halo_n) < len(halomass):
+            halo_indices = np.arange(int(sec * halo_n), int((sec + 1) * halo_n), 1)
         else:
-            halo_indices = np.arange(int(sec*halo_n),len(halomass),1)
-        
-        
-       
-        
-        n_HI_halo_sec = n_HI_halo[halo_indices,:]
-        T_halo_sec = T_halo[halo_indices,:]
-        v_pec_halo_sec = v_pec_halo[halo_indices,:]
-        
-        
-        
-        
-        
-        # Create a mask for zeroing out elements above each index
-        I = (i_center*np.ones(len(halo_indices))).astype(np.int32)
+            halo_indices = np.arange(int(sec * halo_n), len(halomass), 1)
+
+        n_HI_halo_sec   = n_HI_halo[halo_indices, :]
+        T_halo_sec      = T_halo[halo_indices, :]
+        v_pec_halo_sec  = v_pec_halo[halo_indices, :]
+
+        # Apply mask to zero out elements above each index
+        I    = (i_center * np.ones(len(halo_indices))).astype(np.int32)
         mask = np.arange(n_HI_halo_sec.shape[1]) > I[:, None]
-        
-        # Apply the mask to set elements above each index to zero
         n_HI_halo_sec[mask] = 0.0
-        
+
         start_time = time.time()
-        print("tau (dim = " + str (n_HI_halo_sec.shape)+") calc started...")
-        F_halo_los = tau(x_sim,z,n_HI_halo_sec,T_halo_sec,v_pec_halo_sec)
-        print("tau (dim = " + str (F_halo_los.shape)+") calc finished...")
-        end_time = time.time()
-        
-        elapsed_time = end_time - start_time
-        print(f"Elapsed Time: {elapsed_time:.5f} seconds")
-        
-        z_repeats = np.repeat(z[np.newaxis,:],repeats=len(halo_indices),axis=0)
-        vpos_halos = c.to(u.km/u.s).value*(z_repeats - z_centre)/(1+z_centre)
-        
-        
-        # Perform vectorized interpolation using np.interp for each halo
+        print(f"tau (dim = {n_HI_halo_sec.shape}) calculation started...")
+        F_halo_los = tau(x_sim, z, n_HI_halo_sec, T_halo_sec, v_pec_halo_sec)
+        print(f"tau (dim = {F_halo_los.shape}) calculation finished.")
+        elapsed_time = time.time() - start_time
+        print(f"Elapsed Time: {elapsed_time:.2f} seconds")
+
+        z_repeats      = np.repeat(z[np.newaxis, :], repeats=len(halo_indices), axis=0)
+        vpos_halos     = c.to(u.km / u.s).value * (z_repeats - z_centre) / (1 + z_centre)
+
+        # Vectorized interpolation
         F_halo_interpolated = np.array([
             np.interp(vpos_halos_final, vpos_halos[i, :], F_halo_los[i, :], left=np.nan, right=np.nan)
             for i in range(len(halo_indices))
         ])
-        
-        # Assign interpolated values to the final array
-        F_halo_final[sec * halo_n:sec * halo_n + len(halo_indices), :] = F_halo_interpolated
-        
-        del n_HI_halo_sec, T_halo_sec, v_pec_halo_sec
-        
 
-      
-        np.save(file_dir+"/tau_halo_M_9_5_grid_023_v2.npy",F_halo_final)
+        F_halo_final[sec * halo_n:sec * halo_n + len(halo_indices), :] = F_halo_interpolated
+
+        del n_HI_halo_sec, T_halo_sec, v_pec_halo_sec
+
+    # Save results
+    np.save(file_dir + "/tau_halo_M_9_5_grid_023_v2.npy", F_halo_final)
